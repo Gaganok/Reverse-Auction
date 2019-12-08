39af318b3fcc1ef1e0432c53b363dc9b964ceaf4
danielle253 <danielledaly9@gmail.com>
2019-12-08 13:48:54 +0000

    Everything to do with i18n

--- src/main/java/ie/cit/configuration/InternationalizationConfiguration.java
+++ src/main/java/ie/cit/configuration/InternationalizationConfiguration.java
@@ -10,13 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+@SuppressWarnings("deprecation")
 @Configuration
 public class InternationalizationConfiguration extends WebMvcConfigurerAdapter{
 
 	@Bean
 	public LocaleResolver localeResolver() {
 	    SessionLocaleResolver slr = new SessionLocaleResolver();
-	    slr.setDefaultLocale(Locale.US);
+	    slr.setDefaultLocale(Locale.UK);
 	    return slr;
 	}
 	
